239d0b9c024c3e0748c3db3a81feffa3dac055a1
Yu-Ecchuya <ecchuya.bs@gmail.com>
2023-06-18 21:06:42 +0900

    frontend: APIをトークン無しでは取得できないようにする - 非同期処理の調整

--- frontend/src/components/Contents.jsx
+++ frontend/src/components/Contents.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
 import { fetchAPI } from "../services/api";
 
-class Contents extends React.Component {
+class Contents extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
     };
+
+    // 非同期キャンセル用の定義
+    this.abortController = new AbortController();
+    // fetchAPIの動きを検知
+    this.fetchTask = null;
   }
 
   async componentDidMount() {
-    const data = await fetchAPI();
+    this.fetchTask = fetchAPI();
+    const data = await this.fetchTask;
     this.setState({
       data: data
     });
+  }
+
+  componentWillUnmount() {
+    // unmountで非同期を切る
+    this.fetchTask &&  this.abortController.abort();
   }
 
   render() {
